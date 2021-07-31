49aea6815fe5969f11b3318fddcdace437140aba
Fishuke <fishukee@gmail.com>
2021-07-31 21:38:46 +0300

    Add navbar

---
+++ src/components/layout/header/navbar.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Box from '@material-ui/core/Box';
+
+export default function Navbar() {
+    return (
+        <Box sx={{height: '45px', backgroundColor: "primary.dark"}}>
+
+        </Box>
+    );
+}
