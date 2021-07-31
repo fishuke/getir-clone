b6558a547b858b8aba32f005819979a8ff129ff7
Fishuke <fishukee@gmail.com>
2021-07-31 16:16:00 +0300

    Add src folder and move pages & Remove initial codes.

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Getir</title>
+        <meta name="description" content="Getir clone made with NextJs" />
+        <link rel="icon" href="/icons/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
