ad0bc23e6875578080bef613f1811c30c8331ed8
Fishuke <fishukee@gmail.com>
2021-08-01 01:16:25 +0300

    Add nav items

--- src/components/layout/header/navbar.tsx
+++ src/components/layout/header/navbar.tsx
@@ -1,10 +1,31 @@
 import * as React from 'react';
 import Box from '@material-ui/core/Box';
+import Image from 'next/image'
+import styles from "./navbar.module.css";
 
 export default function Navbar() {
     return (
-        <Box sx={{height: '45px', backgroundColor: "primary.dark"}}>
-
+        <Box sx={{height: '45px', backgroundColor: "primary.dark", overflow: 'hidden', paddingRight: "32px", paddingLeft: "32px"}}>
+            <div className={styles.container}>
+                <div className={styles['nav-list']}>
+                    <div className={styles['nav-item']}>
+                        <Image
+                            src="/images/getir.svg"
+                            alt="getir logo"
+                            width={'36px'}
+                            height={16}
+                        />
+                    </div>
+                    <div className={styles['nav-item']}>
+                        <Image
+                            src="/images/getir-yemek.svg"
+                            alt="getir yemek"
+                            width={'87px'}
+                            height={16}
+                        />
+                    </div>
+                </div>
+            </div>
         </Box>
     );
 }
