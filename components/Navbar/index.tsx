029121cc58c86129f39fdd2be6c147408409cd0c
Barış DEMİRCİ <demirci.baris38@gmail.com>
2021-08-02 10:49:13 +0000

    🎨 Better component structure

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,42 @@
+import type { FC } from 'react';
+import { Box } from '@material-ui/core';
+import Image from 'next/image';
+import styles from './navbar.module.css';
+
+export const Navbar: FC = () => {
+    return (
+        <>
+            <Box
+                sx={{
+                    height: '45px',
+                    backgroundColor: 'primary.dark',
+                    overflow: 'hidden',
+                    paddingRight: '32px',
+                    paddingLeft: '32px',
+                }}
+            >
+                <div className={styles.container}>
+                    <div className={styles['nav-list']}>
+                        <div className={styles['nav-item']}>
+                            <Image
+                                src="/images/getir.svg"
+                                alt="getir logo"
+                                width={'36px'}
+                                height={16}
+                            />
+                        </div>
+                        <div className={styles['nav-item']}>
+                            <Image
+                                src="/images/getir-yemek.svg"
+                                alt="getir yemek"
+                                width={'87px'}
+                                height={16}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </Box>
+            <Box sx={{ height: '75px', backgroundColor: 'primary.main' }}></Box>
+        </>
+    );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import '../styles/globals.css';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            dark: '#4c3398',
+            main: '#5d3ebc',
+            light: '#f3f0fe',
+            contrastText: '#fff',
+        },
+        secondary: {
+            main: '#7849f7',
+            contrastText: '#fff',
+        },
+        common: {
+            black: '#191919',
+        },
+        warning: {
+            main: '#ddf300',
+        },
+    },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
-export default MyApp
+
+export default MyApp;
