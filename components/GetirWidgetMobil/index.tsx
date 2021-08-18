2d21be32aa394238d446018ed3d9c7538d30230a
Mert Can Altın <mertgold60@gmail.com>
2021-08-18 12:57:45 +0300

    w-auto

--- components/GetirWidgetMobil/index.tsx
+++ components/GetirWidgetMobil/index.tsx
@@ -6,7 +6,7 @@ export const GetirWidgetMobil: FC = () => {
     return(
         <div className='flex justify-center mobilbanner'>
             <img className='hidden lg:grid desktop-img' src="https://i.hizliresim.com/213hg5o.png" alt="" />
-            <img className='lg:hidden rounded-none' src="https://cdn.getir.com/promos/6117ddb00536373660359720_banner_tr_1628954096460.jpeg" alt="" />
+            <img className='lg:hidden rounded-none w-auto' src="https://cdn.getir.com/promos/6117ddb00536373660359720_banner_tr_1628954096460.jpeg" alt="" />
         </div>
     )
 };
