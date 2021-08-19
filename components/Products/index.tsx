3394ca772db94bc7291db0d82993e4a78ab5cc3e
mertcanaltin <mertgold60@gmail.com>
2021-08-19 23:56:56 +0300

    deleted store,counter

--- components/Products/index.tsx
+++ components/Products/index.tsx
@@ -2,39 +2,29 @@
 import { FC } from 'react';
 import './categorys.module.scss';
 import ProductsData from './product.json';
-import { Counter } from '../productCounter/Counter';
-import { Buttons } from '../productCounter/Buttons';
 
-import {Provider} from 'react-redux';
-
-import store from '../../store';
 const data = ProductsData.products;
 console.log(data);
 
 export const Products: FC = () => {
     return (
-        <Provider store={store}>
+        <div>
             <p className="categorytext hidden lg:grid">Kategoriler</p>
             <div className="flex justify-center">
                 <div className="margintop product">
                     <div className="grid grid-cols-4 lg:grid-cols-4  gap-4">
-                    
                         {data.map((products, index) => (
                             <div key={index} className="col-span-1">
                                 <img
-                                    
                                     src={products.product_img}
                                     alt={products.product_name}
                                 />
                                 <p>{products.product_name}</p>
-                                <Counter />
-                                <Buttons />
                             </div>
-                            
                         ))}
                     </div>
                 </div>
             </div>
-        </Provider>
+        </div>
     );
 };
--- store/types/index.tsx
+++
@@ -1,2 +0,0 @@
-export const URUN_ARTTIR = 'URUN_ARTTIR';
-export const URUN_AZALT = 'URUN_AZALT';
