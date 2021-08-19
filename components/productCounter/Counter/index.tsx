91940037727940cfb952d6025e778c505d0e0660
mertcanaltin <mertgold60@gmail.com>
2021-08-19 23:53:53 +0300

    edited counter.js

--- components/productCounter/Counter/index.tsx
+++ components/productCounter/Counter/index.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react';
 import {useSelector} from 'react-redux'
 
 export const Counter: FC = () => {
-  const {counter} = useSelector(state => state);
+  const counter = useSelector(state => state);
   console.log(counter);
   return(
     <div>{counter}</div>
