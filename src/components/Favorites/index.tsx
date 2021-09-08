a578a4471464adb0402fb751bc9976dd0cea49bc
mertcanaltin <mertgold60@gmail.com>
2021-09-08 19:26:32 +0300

    add product: any

--- src/components/Favorites/index.tsx
+++ src/components/Favorites/index.tsx
@@ -3,8 +3,7 @@ import Products from '../../api/products.json'
 import ProductItem from '../ui/ProductItem'
 export const Favorites: FC = () => {
 
-    
-	const [products, setProducts] = useState([] as any);
+    const [products, setProducts] = useState([] as any);
 
 	useEffect(() => {
 		setProducts(Products)
@@ -13,7 +12,7 @@ export const Favorites: FC = () => {
     return (
         <div className="container mt-16">
 			<div className="overflow-hidden rounded-lg grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8 gap-0.1">
-				{products && products.map(product => <ProductItem product={product}/>)}
+				{products && products.map((product: any) => <ProductItem product={product}/>)}
 			</div>
         </div>
     );
