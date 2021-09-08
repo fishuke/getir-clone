bf957be5a711a83987476436ada9d0d1ae333d09
mertcanaltin <mertgold60@gmail.com>
2021-09-08 19:28:52 +0300

    add product key

--- src/components/Favorites/index.tsx
+++ src/components/Favorites/index.tsx
@@ -12,7 +12,7 @@ export const Favorites: FC = () => {
     return (
         <div className="container mt-16">
 			<div className="overflow-hidden rounded-lg grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8 gap-0.1">
-				{products && products.map((product: any) => <ProductItem product={product}/>)}
+				{products && products.map((product: any) => <ProductItem key={product} product={product}/>)}
 			</div>
         </div>
     );
